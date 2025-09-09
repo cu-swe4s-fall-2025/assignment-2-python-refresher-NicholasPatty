0551dc85d8884f7d4ca84563e25617f9b3668122
Nicholas Patty <nicho@NPs-MacBook.local>
2025-09-08 20:31:19 -0600

    Modified my_utils.py to include updated get_column function

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,15 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    # open file and process it line by line
+    get_column_array = []
+    with open(file_name, 'r') as file:
+        # for each line, split the line into an array
+        for line in file:
+            columns = line.strip().split(',')
+            # check to see if the query_column position matches the query_value
+            # when the above condition is met, add the value in the result_column position into the array
+            if columns[query_column] == query_value:
+                get_column_array.append(columns[result_column])
+    # return the array storing the column values
+    return get_column_array
+
+#print(get_column('Agrofood_co2_emission.csv', 0, 'Afghanistan', 3))
