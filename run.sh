74e05d676d5b209d65ed54f57dbce6b4ca831de5
Nicholas Patty <nicho@NPs-MacBook.local>
2025-09-23 16:38:05 -0600

    Updated run.sh to run 3 versions of print_fires, 1 that works and 2 that give errors

--- run.sh
+++ run.sh
@@ -1,7 +1,43 @@
 #!/bin/bash
 
 echo "running print_fires.py"
-python3 print_fires.py
+
+python_script=print_fires.py
+file_name="Agrofood_co2_emission.csv"
+country_column=0
+country="United States of America"
+fires_column="Forest fires"
+
+echo "-----------------------------------"
+echo "example 1 (successful execution expected):"
+
+python3 "$python_script" \
+--file_name "$file_name" \
+--country_column $country_column \
+--country "$country" \
+--fires_column "$fires_column"
+
+echo "-----------------------------------"
+echo "example 2 (error expected):" 
+file_name='fake.csv'
+
+python3 "$python_script" \
+--file_name "$file_name" \
+--country_column $country_column \
+--country "$country" \
+--fires_column "$fires_column"
+
+echo "-----------------------------------"
+echo "example 3 (error expected):"
+file_name="Agrofood_co2_emission.csv"
+fires_column="oil fire"
+
+python3 "$python_script" \
+--file_name "$file_name" \
+--country_column $country_column \
+--country "$country" \
+--fires_column "$fires_column"
+
 echo "done"
 
 chmod -x run.sh
